e8432151ffb68555224043c595d5dfa8e0b7196d
Cristiano Caon <cristiano.caon@addepar.com>
2023-06-14 17:43:19 -0400

    create creds

--- main.tf
+++ main.tf
@@ -1,52 +1,63 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = "us-east-1"
+  region     = "us-east-1"
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "key_pair"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami = var.ec2_ami_id
-  count = var.ec2_count
-  instance_type = var.ec2_type
+  ami             = var.ec2_ami_id
+  count           = var.ec2_count
+  instance_type   = var.ec2_type
   security_groups = [aws_security_group.ec2_sec_grp.name]
+  key_name        = aws_key_pair.key_pair.key_name
   tags = {
     Name = var.ec2_name
   }
   user_data = templatefile("user_data.yaml", {
-    ssh_private_key = "private key value",
+    ssh_private_key      = "private key value",
     ssh_private_key_file = "private_key.pem"
   })
 }
 
 resource "aws_security_group" "ec2_sec_grp" {
-  name = var.ec2_sec_grp_name
+  name        = var.ec2_sec_grp_name
   description = "Allow HTTP/HTTPS and SSH ingress only from my IP address."
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["99.104.233.184/32"]
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["99.104.233.184/32"]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["99.104.233.184/32"]
   }
 
   egress {
-   from_port   = 0
-   to_port     = 0
-   protocol    = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,28 +1,28 @@
 variable "access_key" {
   description = "Access key to IAM user."
-  sensitive = true
+  sensitive   = true
 }
 variable "secret_key" {
   description = "Secret key to IAM user."
-  sensitive = true
+  sensitive   = true
 }
 variable "ec2_name" {
   description = "Name of the instance."
-  default = "ec2_test"
+  default     = "ec2_test"
 }
 variable "ec2_count" {
   description = "Number of instances."
-  default = 1
+  default     = 1
 }
 variable "ec2_type" {
   description = "Type of EC2 instance."
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 variable "ec2_ami_id" {
   description = "The Amazon Linux AMI."
-  default = "ami-0715c1897453cabd1"
+  default     = "ami-0715c1897453cabd1"
 }
 variable "ec2_sec_grp_name" {
   description = "Name for security group of EC2 instance."
-  default = "ec2_sec_grp"
+  default     = "ec2_sec_grp"
 }
