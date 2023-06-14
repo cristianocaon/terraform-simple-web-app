d2d76a3794c22fa94035253207624d395aeeae29
Cristiano Caon <cristiano.caon@addepar.com>
2023-06-14 17:46:56 -0400

    remove key pair

--- main.tf
+++ main.tf
@@ -4,22 +4,11 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "key_pair" {
-  key_name   = "key_pair"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
 resource "aws_instance" "ec2_instance" {
   ami             = var.ec2_ami_id
   count           = var.ec2_count
   instance_type   = var.ec2_type
-  security_groups = [aws_security_group.ec2_sec_grp.name]
-  key_name        = aws_key_pair.key_pair.key_name
+  security_groups = [aws_security_group.sec_grp.name]
   tags = {
     Name = var.ec2_name
   }
@@ -29,7 +18,7 @@ resource "aws_instance" "ec2_instance" {
   })
 }
 
-resource "aws_security_group" "ec2_sec_grp" {
+resource "aws_security_group" "sec_grp" {
   name        = var.ec2_sec_grp_name
   description = "Allow HTTP/HTTPS and SSH ingress only from my IP address."
 
