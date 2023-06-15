0e4f5a17a2c2950ec8450f00128962bdbb6764db
Cristiano Caon <cristiano.caon@addepar.com>
2023-06-15 10:02:08 -0400

    add validation

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2_instance" {
   }
   user_data = templatefile("user_data.yaml", {
     ssh_private_key      = "private key value",
-    ssh_private_key_file = "private_key.pem"
+    ssh_private_key_file = "/home/ec2-user/private_key.pem"
   })
   user_data_replace_on_change = true
 }
